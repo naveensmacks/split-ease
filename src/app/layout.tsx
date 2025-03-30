a9b0f6169ddebc942c985339c8afd34955d30756
naveensmacks <naveen.ilearn@gmail.com>
2025-03-30 14:54:31 +0530

    Added favicon and seo in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,23 @@ import "./globals.css";
 
 const font = Quicksand({ subsets: ["latin"] });
 
+// export const metadata: Metadata = {
+//   title: "Split Ease",
+//   description: "Split and ease your expenses.",
+// };
+
 export const metadata: Metadata = {
-  title: "Split Ease",
-  description: "Split and ease your expenses.",
+  title: "SplitEase - Free Expense Splitter",
+  description: "View and manage your expense groups with ease.",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: "SplitEase - Free Expense Splitter",
+    description: "View and manage your expense groups with ease.",
+    url: "https://app.splitease.net/app/groups",
+    images: ["https://splitease.net/social-preview.png"],
+  },
 };
 
 export default function RootLayout({
